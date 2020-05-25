09fe218e387bd6076847980cda826e3950a8bab9
ekincan.casim <ekincan.casim@ECASIM.postr.local>
2020-05-25 15:58:19 +0300

    Changed @Repository annotation to @Service

--- src/main/java/com/eccsm/service/APIService.java
+++ src/main/java/com/eccsm/service/APIService.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
 
 import com.eccsm.auth.JwtTokenUtil;
@@ -21,7 +21,7 @@ import com.eccsm.validator.CategoryValidator;
 import com.eccsm.validator.ItemValidator;
 
 @Transactional
-@Repository
+@Service
 @SuppressWarnings({ "unchecked", "rawtypes" })
 public class APIService {
 
